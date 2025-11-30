679391a990bdc0a49a84c988429ae443dafe028d
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-30 17:03:30 +0000

    m

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,6 +3,14 @@
 
 
 def list_all(mongo_collection):
-    """aa"""
+    """
+    lists all documents in a MongoDB collection.
+
+    args:
+        mongo_collection: The pymongo collection object to query.
+
+    returns:
+        A list of documents (dictionaries), or an empty list if none found.
+    """
 
     return list(mongo_collection.find())
