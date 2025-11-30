2b0d81f463f5f8227df0c1316790cd8d13bd8a6c
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-30 17:17:53 +0000

    m

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -14,7 +14,7 @@ def update_topics(mongo_collection, name, topics):
 
     mongo_collection.update_many(
     {"name": name},
-    {$set: {"topics": topics}}
+    {$set: {"topics": topics}},
     {multi: true}
             )
 
