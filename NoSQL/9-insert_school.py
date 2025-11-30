4cc06d4b7ee12061be91c5f2c19a7aa12009b9da
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-30 17:09:19 +0000

    m

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env/ python3
+"""9-insert_school.py"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a collection based on kwargs.
+
+    args:
+        mongo_collection: the pymongo collection object.
+        **kwargs: arbitrary keyword arguments to be inserted as the document.
+
+    returns:
+        the _id of the newly inserted document.
+    """
+    result = mongo_collection.insert_one(kwargs)
+
+    return result.inserted_id
