7d2b8e1f3132a7edfedc50a104c04c370127cb21
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-30 17:26:35 +0000

    m

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
-"""11-schools_by_topic.py11-schools_by_topic.py"""
+"""
+this module defines a function to find documents based on a list item.
+"""
 
 
 def schools_by_topic(mongo_collection, topic):
-    """def schools_by_topic(mongo_collection, topic):def schools_by_topic(mongo_collection, topic):def schools_by_topic(mongo_collection, topic):"""
+    """
+    returns the list of schools having a specific topic.
+
+    args:
+        mongo_collection: the pymongo collection object.
+        topic (str): the topic to search for.
+
+    returns:
+        list: A list of dictionaries (documents) matching the topic.
+    """
 
     search = {"topic": topic}
     return mongo_collection.find(search)
